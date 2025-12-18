279478c0d5d103378b81b8bf1f564bd5dcf24f0a
netsyncmali <mali@netsync.com>
2025-12-18 08:24:37 +0200

    refactor: update non-DRM stream sources and improve organization

--- src/app/services/stream.service.ts
+++ src/app/services/stream.service.ts
@@ -23,43 +23,7 @@ export interface StreamSource {
 })
 export class StreamService {
   readonly streams: StreamSource[] = [
-    // === Clear (Non-DRM) Streams - These always work ===
-    {
-      name: 'Big Buck Bunny (DASH)',
-      url: 'https://storage.googleapis.com/shaka-demo-assets/bbb-dark-truths/dash.mpd',
-      type: 'dash',
-      description: 'Shaka official - Big Buck Bunny'
-    },
-    {
-      name: 'Angel One (DASH)',
-      url: 'https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd',
-      type: 'dash',
-      description: 'Shaka demo - multilingual with subtitles'
-    },
-    {
-      name: 'Angel One (HLS)',
-      url: 'https://storage.googleapis.com/shaka-demo-assets/angel-one-hls/hls.m3u8',
-      type: 'hls',
-      description: 'Shaka demo asset - HLS format'
-    },
-    {
-      name: 'Sintel 4K (DASH)',
-      url: 'https://storage.googleapis.com/shaka-demo-assets/sintel/dash.mpd',
-      type: 'dash',
-      description: 'Fantasy animated short - 4K'
-    },
-    {
-      name: 'Tears of Steel (DASH)',
-      url: 'https://storage.googleapis.com/shaka-demo-assets/tos-ttml/dash.mpd',
-      type: 'dash',
-      description: 'Sci-fi short with TTML subtitles'
-    },
-    {
-      name: 'Heliocentrism (DASH)',
-      url: 'https://storage.googleapis.com/shaka-demo-assets/heliocentrism/heliocentrism.mpd',
-      type: 'dash',
-      description: 'Multi-period DASH demo'
-    },
+    // === Clear (Non-DRM) Streams ===
     {
       name: 'DASH-IF Big Buck Bunny',
       url: 'https://dash.akamaized.net/dash264/TestCases/1c/qualcomm/2/MultiRate.mpd',
@@ -72,20 +36,7 @@ export class StreamService {
       type: 'dash',
       description: 'DASH-IF live stream simulator'
     },
-    {
-      name: 'Art of Motion (DASH)',
-      url: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd',
-      type: 'dash',
-      description: 'Bitmovin demo content'
-    },
-    {
-      name: 'Art of Motion (HLS)',
-      url: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
-      type: 'hls',
-      description: 'Bitmovin HLS demo'
-    },
-    // === DRM Protected Streams ===
-    // ClearKey DRM - works in all modern browsers without external license servers
+    // === DRM Protected Streams (ClearKey) ===
     {
       name: '🔒 Axinom ClearKey (DASH)',
       url: 'https://media.axprod.net/TestVectors/v7-MultiDRM-SingleKey/Manifest_1080p_ClearKey.mpd',
